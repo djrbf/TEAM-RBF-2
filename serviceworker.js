69e263052356b5998519c32eabdc5a02a56ec0ff
DJ RBF <godjj88@gmail.com>
2025-07-31 13:55:17 +0200

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,26 +1,26 @@
 
-const CACHE_NAME = "djrbf-cache-v1";
-const urlsToCache = [
-  "/TEAM-RBF-2/",
-  "/TEAM-RBF-2/index.html",
-  "/TEAM-RBF-2/style.css",
-  "/TEAM-RBF-2/video1.mp4",
-  "/TEAM-RBF-2/video2.mp4",
-  "/TEAM-RBF-2/video3.mp4",
-  "/TEAM-RBF-2/video4.mp4",
-  "/TEAM-RBF-2/icon-512x512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('djrbf-store').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/manifest.json',
+                '/serviceworker.js',
+                '/video/video1.mp4',
+                '/video/video2.mp4',
+                '/video/video3.mp4',
+                '/video/video4.mp4',
+                '/icons/icon-512x512.png'
+            ]);
+        })
+    );
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
 });
